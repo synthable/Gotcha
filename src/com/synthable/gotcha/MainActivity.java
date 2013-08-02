3a3c8ffa73229388d2f1c52a95252e78c6da2a27
Jason Knight <jcknight@gmail.com>
2013-08-02 11:55:47 -0700

    Support for Maps v2

--- src/com/synthable/gotcha/MainActivity.java
+++ src/com/synthable/gotcha/MainActivity.java
@@ -1,10 +1,24 @@
 package com.synthable.gotcha;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapLongClickListener, OnMapClickListener {
+
+	private GoogleMap mMap;
+	private int mMarkerCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +26,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded(); 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +38,64 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            		.getMap();
+
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+    	LatLng point = new LatLng(38.791006, -121.237299);
+
+        mMap.addMarker(new MarkerOptions().position(point).title("Marker"));
+        mMap.addCircle(new CircleOptions()
+	        .center(point)
+	        .radius(100)
+	        .strokeWidth(2)
+	        .fillColor(Color.BLUE)
+        );
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(point, 15);
+        mMap.animateCamera(cameraUpdate);
+
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMapClickListener(this);
+
+        mMap.setMyLocationEnabled(true);
+    }
+
+	@Override
+	public void onMapLongClick(LatLng point) {
+		if(mMap != null) {
+			mMap.addMarker(new MarkerOptions().position(point).title("Marker "+ ++mMarkerCounter));
+			mMap.addCircle(new CircleOptions()
+		        .center(point)
+		        .radius(25)
+		        .strokeWidth(2)
+		        .fillColor(Color.RED)
+			);
+		}
+	}
+
+	@Override
+	public void onMapClick(LatLng point) {
+		if(mMap != null) {
+			mMap.addMarker(new MarkerOptions().position(point).title("Marker "+ ++mMarkerCounter));
+			mMap.addCircle(new CircleOptions()
+		        .center(point)
+		        .radius(10)
+		        .strokeWidth(2)
+		        .fillColor(Color.YELLOW)
+			);
+		}
+	}
 }
