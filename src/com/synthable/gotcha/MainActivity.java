d7624a544a4f592f938d9c42d83ea18091a107d5
Jason Knight <jcknight@gmail.com>
2013-08-02 14:24:28 -0700

    Lots of boiler plate Location Service code

--- src/com/synthable/gotcha/MainActivity.java
+++ src/com/synthable/gotcha/MainActivity.java
@@ -1,10 +1,19 @@
 package com.synthable.gotcha;
 
+import android.content.IntentSender;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,8 +24,20 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapLongClickListener, OnMapClickListener {
+public class MainActivity extends FragmentActivity implements
+	LocationListener, OnMapLongClickListener, OnMapClickListener,
+	ConnectionCallbacks, OnConnectionFailedListener {
 
+	private static final int MILLISECONDS_PER_SECOND = 1000;
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 2;
+    private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 2;
+    private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
+    private static final int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+	private LocationRequest mLocationRequest;
+	private LocationClient mLocationClient;
+	private Location mCurrentLocation;
 	private GoogleMap mMap;
 	private int mMarkerCounter;
 
@@ -24,13 +45,34 @@ public class MainActivity extends FragmentActivity implements OnMapLongClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLocationClient = new LocationClient(this, this, this);
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
     }
+
+    @Override
+	protected void onStart() {
+		super.onStart();
+		
+		mLocationClient.connect();
+	}
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded(); 
+        setUpMapIfNeeded();
     }
+
+    @Override
+	protected void onStop() {
+    	mLocationClient.disconnect();
+
+		super.onStop();
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,16 +87,11 @@ public class MainActivity extends FragmentActivity implements OnMapLongClickList
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
             		.getMap();
-
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
         }
     }
 
     private void setUpMap() {
-    	LatLng point = new LatLng(38.791006, -121.237299);
+    	LatLng point = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
 
         mMap.addMarker(new MarkerOptions().position(point).title("Marker"));
         mMap.addCircle(new CircleOptions()
@@ -98,4 +135,80 @@ public class MainActivity extends FragmentActivity implements OnMapLongClickList
 			);
 		}
 	}
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        mCurrentLocation = location;
+    }
+
+    /*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+
+        mCurrentLocation = mLocationClient.getLastLocation();
+
+        // Check if we were successful in obtaining the map.
+        if (mMap != null) {
+        	setUpMap();
+        }
+    }
+ 
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+    }
+ 
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            //showErrorDialog(connectionResult.getErrorCode());
+        	Toast.makeText(this, "Shit happened..."+ connectionResult.getErrorCode(), Toast.LENGTH_SHORT).show();
+        	finish();
+        }
+    }
 }
